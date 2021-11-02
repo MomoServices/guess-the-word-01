99c2ba1453609204b1c3f1a573adb8e73134d181
morayo_o <morayobusinesses@gmail.com>
2021-11-01 21:08:56 -0700

    lessong two on guess the game

--- js/script.js
+++ js/script.js
@@ -27,3 +27,26 @@ guessLetterButton.addEventListener("click", function (e) {
   console.log(guess);
   letterInput.value = "";
 });
+
+const validateInput = function (input) {
+  const acceptedLetter = /[a.zA-Z]/;
+  if (input.length === 0) {
+    message.innerText = "Please enter a letter.";
+  } else if (input.length > 1) {
+    message.innerText = "Please enter a single letter.";
+  } else if (!input.match(acceptedLetter)) {
+    message.innerText = "Please enter a letter from A to Z.";
+  } else {
+    return input;
+  }
+};
+
+const makeGuess = function (guess) {
+  guess = guess.toUpperCase();
+  if (guessedLettersElement.includes(guess)) {
+    message.innerText = "You already guessed that letter. Try again.";
+  } else {
+    guessedLetters.push(guess);
+    console.log(guessedLetters);
+  }
+}
